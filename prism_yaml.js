d8754ad7bd2af6fe20898857d50c523e0c2cd46a
Pablo López // plopesc <plopesc@gmail.com>
2015-05-03 13:24:17 +0200

    Initial commit

---
+++ prism_yaml.js
@@ -0,0 +1,21 @@
+Prism.languages.yaml = {
+  'property': /(\b|\B)[\w-]+(?=\s*:)/ig,
+  'punctuation': /[\{\};:]/g
+};
+
+//Prism.languages.css = {
+//  'comment': /\/\*[\w\W]*?\*\//g,
+//  'atrule': {
+//    pattern: /@[\w-]+?.*?(;|(?=\s*{))/gi,
+//    inside: {
+//      'punctuation': /[;:]/g
+//    }
+//  },
+//  'url': /url\((["']?).*?\1\)/gi,
+//  'selector': /[^\{\}\s][^\{\};]*(?=\s*\{)/g,
+//  'property': /(\b|\B)[\w-]+(?=\s*:)/ig,
+//  'string': /("|')(\\?.)*?\1/g,
+//  'important': /\B!important\b/gi,
+//  'ignore': /&(lt|gt|amp);/gi,
+//  'punctuation': /[\{\};:]/g
+//};
